3401cb57aa3102788dad094434b6c267a941f318
Dillon Streator <dillonstreator@gmail.com>
2017-05-08 21:07:21 -0500

    add all namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      get "/students" => "students#index"
+      post "/students" => "students#create"
+      get "/students/:id" => "students#show"
+      patch "/students/:id" => "students#update"
+      delete "/students/:id" => "students#destroy"
+
+      get "/capstones" => "capstones#index"
+      post "/capstones" => "capstones#create"
+      get "/capstones/:id" => "capstones#show"
+      patch "/capstones/:id" => "capstones#update"
+      delete "/capstones/:id" => "capstones#destroy"
+
+      get "/educations" => "educations#index"
+      post "/educations" => "educations#create"
+      get "/educations/:id" => "educations#show"
+      patch "/educations/:id" => "educations#update"
+      delete "/educations/:id" => "educations#destroy"
+
+      get "/skills" => "skills#index"
+      post "/skills" => "skills#create"
+      get "/skills/:id" => "skills#show"
+      patch "/skills/:id" => "skills#update"
+      delete "/skills/:id" => "skills#destroy"
+      
+    end
+  end
+
 end
