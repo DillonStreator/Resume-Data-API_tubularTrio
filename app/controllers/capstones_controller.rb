ea0a00bb208312ddfa5ae63ff3e7a5c4e8877470
Erika Quinn <quinnerika16@gmail.com>
2017-05-09 20:56:50 -0500

    added params to Capstones controller

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,22 +1,41 @@
 class CapstonesController < ApplicationController
 
-  def index 
+  def index
+    @capstones = Capstone.all 
     render "index.json.jbuilder"
   end
 
-  def show 
+  def show
+    @capstones = Capstone.find_by(id: params[:id]) 
     render "show.json.jbuilder"
   end 
 
-  def create 
-    render "create.json.jbuilder"
+  def create
+    @capstone = Capstone.new(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot_url: params[:screenshot_url],
+      student_id: params[:student_id]
+    )
+    @capstone.save
+    redirect_to "/resumes"
   end
 
   def update
+    @capstone = Capstone.find_by(id: params[:id])
+    @capstone.name = params[:name] || @capstone.name
+    @capstone.description = params[:description] || @capstone.description
+    @capstone.url = params[:url] || @capstone.url
+    @capstone.screenshot_url = params[:screenshot_url] || @capstone.screenshot_url
+    @capstone.student_id = params[:student_id] || @capstone.student_id
+    @capstone.save
     render "update.json.jbuilder"
   end 
 
   def destroy
-    render "destroy.json.jbuilder"
+    @capstone = Capstone.find_by(id: params[:id])
+    @capstone.destroy
+    redirect_to "/resumes"
   end
 end
