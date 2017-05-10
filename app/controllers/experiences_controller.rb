7741481a1ae764a25b66d00239a6213ca3516fed
Jay Lockridge <jklockri@pnw.edu>
2017-05-09 20:44:30 -0500

    Add Create update and destroy to experience controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,22 +1,44 @@
 class ExperiencesController < ApplicationController
  
   def index 
+    @experience=Experience.all
     render "index.json.jbuilder"
   end
 
   def show 
+    @experience = Experience.find_by(id: params[:id])
     render "show.json.jbuilder"
   end 
 
   def create 
+       @experience = Experience.new(
+      start_date: params[:start_date],
+       end_date: params[:end_date],
+       job_title: params[:job_title],
+       company_name: params[:company_name],
+       details: params[:details],
+       student_id: params[:student_id]
+     )
+     @experience.save
     render "create.json.jbuilder"
   end
 
   def update
-    render "update.json.jbuilder"
+     @experience = Experience.find_by(id: params[:id])
+     @experience.start_date = params[:start_date] || @experience.start_date
+     @experience.end_date = params[:end_date] || @experience.end_date
+     @experience.job_title = params[:job_title] || @experience.job_title
+     @experience.company_name = params[:company_name] || @experience.company_name
+     @experience.details = params[:details] || @experience.details 
+     @experience.student_id = params[:student_id] || @experience.student_id
+     @experience.save
+     render "show.json.jbuilder"
+   
   end 
 
   def destroy
+    @experience = Experience.find_by(id: params[:id])
+    @experience.destroy
     render "destroy.json.jbuilder"
   end
   
