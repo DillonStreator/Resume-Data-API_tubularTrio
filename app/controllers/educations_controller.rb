08ff35a2f6d5a00f6400293ee981e1333da61bdd
Dillon Streator <dillonstreator@gmail.com>
2017-05-09 20:49:01 -0500

    fixed educations controller

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,22 +1,32 @@
 class EducationsController < ApplicationController
 
-  def index 
-    render "index.json.jbuilder"
-  end
-
-  def show 
-    render "show.json.jbuilder"
-  end 
-
-  def create 
-    render "create.json.jbuilder"
+  def create
+    @education = Education.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      degree: params[:degree],
+      university_name: params[:university_name],
+      details: params[:details],
+      student_id: params[:student_id]
+    )
+    @education.save
+    redirect_to "/resumes"
   end
 
   def update
-    render "update.json.jbuilder"
+    education = Education.find_by(id: params[:id])
+    education.start_date = params[:start_date] || education.start_date
+    education.end_date = params[:end_date] || education.end_date
+    education.degree = params[:degree] || education.degree
+    education.university_name = params[:university_name] || education.university
+    education.details = params[:details] || education.details
+    redirect_to "/resumes"
   end 
 
   def destroy
-    render "destroy.json.jbuilder"
+    education = Education.find_by(id: params[:id])
+    education.destroy
+    redirect_to "/resumes"
   end
+  
 end
